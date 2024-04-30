8e88d285b7cda79b29813bf9cdae979c7d1d7147
Dipesh Paudel <thedeepyash7@gmail.com>
2024-04-30 19:26:28 +0545

    implement class-based view with serializer

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from watchlist_app.api.views import movie_list, movie_detail
+from watchlist_app.api.views import MovieListAV, MovieDetailAV
 
 app_name = 'watchlist'
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>', movie_detail, name='movie-detail'),
+    path('list/', MovieListAV.as_view(), name='movie-list'),
+    path('<int:pk>', MovieDetailAV.as_view(), name='movie-detail'),
 ]
