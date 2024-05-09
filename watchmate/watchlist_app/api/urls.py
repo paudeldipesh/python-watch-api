9b08d781a2ffa7836e3840fda650106e620a397f
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-09 09:26:35 +0545

    add custom calculation for reviews

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -16,9 +16,7 @@ urlpatterns = [
     path("list/", WatchListAV.as_view(), name="watch-list"),
     path("<int:pk>/", WatchDetailAV.as_view(), name="watch-detail"),
     path("", include(router.urls)),
-    path(
-        "stream/<int:pk>/review-create/", ReviewCreate.as_view(), name="review-create"
-    ),
-    path("stream/<int:pk>/review/", ReviewList.as_view(), name="review-list"),
-    path("stream/review/<int:pk>/", ReviewDetail.as_view(), name="review-detail"),
+    path("<int:pk>/create-review/", ReviewCreate.as_view(), name="review-create"),
+    path("<int:pk>/reviews/", ReviewList.as_view(), name="review-list"),
+    path("review/<int:pk>/", ReviewDetail.as_view(), name="review-detail"),
 ]
