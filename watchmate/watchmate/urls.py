3f5037253b1ab9e8b39b4753d131897941513b1a
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-10 10:39:08 +0545

    add basic and token authentication

--- watchmate/watchmate/urls.py
+++ watchmate/watchmate/urls.py
@@ -21,5 +21,4 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("watch/", include("watchlist_app.api.urls")),
-    path("api-auth/", include("rest_framework.urls")),
 ]
