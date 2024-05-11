45292b7611041f5e095d807ac0f79c3cc7ff8033
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-11 21:56:14 +0545

    add logout functionality

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -1,8 +1,16 @@
+from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from user_app.api.serializers import RegistrationSerializer
 from user_app.models import create_auth_token
+
+
+@api_view(["POST"])
+def logout_view(request):
+    if request.method == "POST":
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 
 @api_view(["POST"])
