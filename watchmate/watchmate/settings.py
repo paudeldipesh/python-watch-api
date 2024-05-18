8cb3497494f4a7bd0954e6625ff9a57f6c00e0df
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-18 23:39:49 +0545

    shift to token authentication

--- watchmate/watchmate/settings.py
+++ watchmate/watchmate/settings.py
@@ -134,11 +134,11 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         # "rest_framework.authentication.BasicAuthentication",
         # "rest_framework.authentication.SessionAuthentication",
-        # "rest_framework.authentication.TokenAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
 
-SIMPLE_JWT = {
-    "ROTATE_REFRESH_TOKENS": True,
-}
+# SIMPLE_JWT = {
+#     "ROTATE_REFRESH_TOKENS": True,
+# }
