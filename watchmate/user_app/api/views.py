8731ca073c43c8ed012daf591157239e4fc8eb72
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-18 07:08:21 +0545

    add simple jwt for authentication and authorization

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from user_app.api.serializers import RegistrationSerializer
-from user_app.models import create_auth_token
+
+# from rest_framework.authtoken.models import Token
+# from user_app import create_auth_token
 
 
 @api_view(["POST"])
@@ -26,8 +28,15 @@ def registration_view(request):
             data["username"] = account.username
             data["email"] = account.email
 
-            token = Token.objects.get(user=account).key
-            data["token"] = token
+            # token = Token.objects.get(user=account).key
+            # data["token"] = token
+
+            refresh = RefreshToken.for_user(account)
+            data["token"] = {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+
         else:
             data = serializer.errors
 
