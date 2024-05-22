e01106ac64f8fd2436a84164781d3f52e8d35fb6
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-22 08:07:32 +0545

    test registration api

--- watchmate/user_app/tests.py
+++ watchmate/user_app/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class RegisterTestCase(APITestCase):
+    def test_register(self):
+        data = {
+            "username": "testcase",
+            "email": "testcase@gmail.com",
+            "password": "testcase",
+            "confirm_password": "testcase",
+        }
+        response = self.client.post(reverse("register"), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
