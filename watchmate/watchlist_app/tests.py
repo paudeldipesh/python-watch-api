6f74530983da91c9b57009c7a0b7ce1a9524751d
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-23 08:47:42 +0545

    test user review api

--- watchmate/watchlist_app/tests.py
+++ watchmate/watchlist_app/tests.py
@@ -176,3 +176,7 @@ class ReviewTestCase(APITestCase):
     def test_review_individual_delete(self):
         response = self.client.delete(reverse("review-detail", args=(self.review.id,)))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_review_user(self):
+        response = self.client.get(f"/watch/reviews/?username={self.user.username}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
